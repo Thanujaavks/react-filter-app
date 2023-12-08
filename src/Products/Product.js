f3cc05e1fa3c0e389f9e6ff939d0994d0cd954c7
Thanujaavks <vksthanujaa@gmail.com>
2023-12-08 23:42:19 +0530

    product

--- src/Products/Product.js
+++ src/Products/Product.js
@@ -25,7 +25,76 @@ function Product() {
                                 <del>$300</del>200
                             </div>
                             <div className={"bag"}>
-                                <BsFillBagHeartFill />
+                                <BsFillBagHeartFill className={"bag-icon"} />
+                            </div>
+                        </section>
+                    </div>
+                </section>
+                <section className={"card"}>
+                    <img src={"https://m.media-amazon.com/images/I/519MRhRKGFL._AC_UX575_.jpg"} alt={"shoe"} className={"card-image"}/>
+                    <div className={"card-details"}>
+                        <h3 className={"card-title"}>
+                            Shoe
+                        </h3>
+                        <section className={"card-reviews"}>
+                            <FaStar className={"rating-star"}/>
+                            <FaStar className={"rating-star"}/>
+                            <FaStar className={"rating-star"}/>
+                            <FaStar className={"rating-star"}/>
+                            <span className={"total-review"}></span>
+                        </section>
+                        <section className={"card-price"}>
+                            <div className={"price"}>
+                                <del>$300</del>200
+                            </div>
+                            <div className={"bag"}>
+                                <BsFillBagHeartFill className={"bag-icon"} />
+                            </div>
+                        </section>
+                    </div>
+                </section>
+                <section className={"card"}>
+                    <img src={"https://m.media-amazon.com/images/I/51+P9uAvb1L._AC_UY695_.jpg"} alt={"shoe"} className={"card-image"}/>
+                    <div className={"card-details"}>
+                        <h3 className={"card-title"}>
+                            Shoe
+                        </h3>
+                        <section className={"card-reviews"}>
+                            <FaStar className={"rating-star"}/>
+                            <FaStar className={"rating-star"}/>
+                            <FaStar className={"rating-star"}/>
+                            <FaStar className={"rating-star"}/>
+                            <span className={"total-review"}></span>
+                        </section>
+                        <section className={"card-price"}>
+                            <div className={"price"}>
+                                <del>$300</del>200
+                            </div>
+                            <div className={"bag"}>
+                                <BsFillBagHeartFill className={"bag-icon"} />
+                            </div>
+                        </section>
+                    </div>
+                </section>
+                <section className={"card"}>
+                    <img src={"https://m.media-amazon.com/images/I/71oEKkghg-L._AC_UX575_.jpg"} alt={"shoe"} className={"card-image"}/>
+                    <div className={"card-details"}>
+                        <h3 className={"card-title"}>
+                            Shoe
+                        </h3>
+                        <section className={"card-reviews"}>
+                            <FaStar className={"rating-star"}/>
+                            <FaStar className={"rating-star"}/>
+                            <FaStar className={"rating-star"}/>
+                            <FaStar className={"rating-star"}/>
+                            <span className={"total-review"}></span>
+                        </section>
+                        <section className={"card-price"}>
+                            <div className={"price"}>
+                                <del>$300</del>200
+                            </div>
+                            <div className={"bag"}>
+                                <BsFillBagHeartFill className={"bag-icon"} />
                             </div>
                         </section>
                     </div>
