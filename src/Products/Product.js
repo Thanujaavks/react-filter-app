6e13ba4f98ff18e417837b558bce0bc5e564cb1e
Thanujaavks <vksthanujaa@gmail.com>
2023-12-08 20:59:40 +0530

    product

--- src/Products/Product.js
+++ src/Products/Product.js
@@ -1,20 +1,32 @@
 import React from 'react'
 import "./Product.css"
 import { FaStar } from "react-icons/fa";
+import { BsFillBagHeartFill } from "react-icons/bs";
 
 function Product() {
     return (
         <>
             <section className={"card-container"}>
                 <section className={"card"}>
-                    <img src={"https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"} alt={"shoe"}/>
+                    <img src={"https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"} alt={"shoe"} className={"card-image"}/>
                     <div className={"card-details"}>
                         <h3 className={"card-title"}>
                             Shoe
                         </h3>
                         <section className={"card-reviews"}>
-                            <FaStar /><FaStar /><FaStar /><FaStar />
+                            <FaStar className={"rating-star"}/>
+                            <FaStar className={"rating-star"}/>
+                            <FaStar className={"rating-star"}/>
+                            <FaStar className={"rating-star"}/>
                             <span className={"total-review"}></span>
+                        </section>
+                        <section className={"card-price"}>
+                            <div className={"price"}>
+                                <del>$300</del>200
+                            </div>
+                            <div className={"bag"}>
+                                <BsFillBagHeartFill />
+                            </div>
                         </section>
                     </div>
                 </section>
